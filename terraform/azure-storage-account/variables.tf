f304b7eb8bfa2ab68f370951ea1308e2a92ee3fd
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-07-19 14:44:56 +0100

    Create variables.tf

---
+++ terraform/azure-storage-account/variables.tf
@@ -0,0 +1,38 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "myTFResourceGroup"
+  description = "RG name in Azure"
+}
+
+variable "location" {
+  type        = string
+  default     = "westus2"
+  description = "RG location in Azure"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage Account name in Azure"
+  default     = "demo"
+}
+
+variable "storage_resource_group" {
+  type        = string
+  description = "Storage Account resource group in Azure"
+  default     = "demo"
+}
+
+variable "port_run_id" {
+  type        = string
+  description = "The runID of the action run that created the entity"
+}
+
+variable "port_client_id" {
+  type        = string
+  description = "The Port client ID"
+}
+
+variable "port_client_secret" {
+  type        = string
+  description = "The Port client secret"
+}
