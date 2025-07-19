c3233a26948dec89db649246d96da68632598ea9
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-07-19 14:48:26 +0100

    Update variables.tf

--- terraform/azure-storage-account/variables.tf
+++ terraform/azure-storage-account/variables.tf
@@ -1,9 +1,3 @@
-variable "resource_group_name" {
-  type        = string
-  default     = "myTFResourceGroup"
-  description = "RG name in Azure"
-}
-
 variable "location" {
   type        = string
   default     = "westus2"
