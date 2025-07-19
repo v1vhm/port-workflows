7702a0018f8b3cb7b3bdbecc5e8294a4a6375b04
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-07-19 14:48:54 +0100

    Update main.tf

--- terraform/azure-storage-account/main.tf
+++ terraform/azure-storage-account/main.tf
@@ -26,7 +26,7 @@ provider "port" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.storage_resource_group
 
   location                 = var.location
   account_tier             = "Standard"
@@ -44,7 +44,7 @@ resource "port_entity" "azure_storage_account" {
     string_props = {
       "storage_name"           = var.storage_account_name,
       "storage_location"       = var.location,
-      "storage_resource_group" = var.resource_group_name
+      "storage_resource_group" = var.storage_resource_group
       "endpoint"               = azurerm_storage_account.storage_account.primary_web_endpoint
     }
   }
